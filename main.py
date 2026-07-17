3dabdbc407d663f68e71d4065bea3daab1d65ab4
Gemma Wright <gemmajwright@googlemail.com>
2026-07-17 14:54:24 +0100

    Tidying

--- main.py
+++ main.py
@@ -15,10 +15,55 @@ def find_task(tasks, task_id):
         raise ValueError(f"No task found with id {task_id}")
     return task
 
+def find_previous_dot_task(tasks, latest_completed_task=None):
+    """ previous_dot_task is the dotted task before the latest completed task.
 
+        Returns previous_dot_task from the task list.
+    """
+    if latest_completed_task == None:
+        # TODO: sort out edge case
+        raise ValueError("No latest_completed_task")
+    if latest_completed_task.is_completed == False:
+        raise ValueError(f"{latest_completed_task.name} has not been completed.")
+
+    for task in tasks[latest_completed_task.index -1::-1]:
+        if task.is_completed == True:
+            continue
+        elif task.is_completed == False and task.is_dotted == False:
+            continue
+        elif task.is_completed == False and task.is_dotted == True:
+            previous_dot_task = task
+            return(previous_dot_task)
+        else:
+            raise ValueError(f"Task {task.name} metadata corrupted")
+    
+
+def find_resume_from_task(tasks, latest_completed_task = None):
+    """ resume_from_task is the next uncompleted, undotted task on the list after the latest completed task.
+        
+        Returns resume_from_task from the task list. 
+    """
+    if latest_completed_task == None:
+        # TODO: sort out edge case
+        raise ValueError("No latest_completed_task")
+    if latest_completed_task.is_completed == False:
+        raise ValueError(f"{latest_completed_task.name} has not been completed.")
+
+    for task in tasks[latest_completed_task.index:]:
+        if task.is_completed == False and task.is_dotted == False:
+            resume_from_task = task
+            return(resume_from_task)
+        elif task.is_completed == False and task.is_dotted == True:
+            raise ValueError(f"Task {task.name} is dotted but not completed, and a task was completed before it.")
+        elif task.is_completed == True:
+            continue
+        else:
+            raise ValueError(f"Task {task.name} metadata corrupted")
+        
 def task_compare(tasks, comparator_task = None):
     """ Compares a comparator task with the next suitable task.
         The comparator and next suitable task will change.
+
         Doesn't return anything but changes the tasks' metadata in the list
     """
     # TODO: resume comparing after completion. Agreed names:
@@ -49,6 +94,11 @@ def task_compare(tasks, comparator_task = None):
 
 
 def task_complete(tasks):
+    """Shows the task ready to be completed, and if the user inputs that they have completed it, 
+        marks it as completed in the metadata. 
+    
+        Returns latest_completed_task.
+    """
     print("Running task_complete...")
     dotted_tasks: list[Task] = []
     for task in tasks:
@@ -66,55 +116,14 @@ def task_complete(tasks):
         print("Please enter y or n")
     if choice == "y":
         latest_dot_task.is_completed = True
-        return latest_dot_task
+        latest_completed_task = latest_dot_task
+        return latest_completed_task
 
     else: # needs more feature! Currently does not return anything
         latest_dot_task = None
         print("App terminated - do the task then run again!")
 
-def find_previous_dot_task(tasks, latest_completed_task=None):
-    """ Returns previous_dot_task from the task list
-    """
-    if latest_completed_task == None:
-        # TODO: sort out edge case
-        raise ValueError("No latest_completed_task")
-    if latest_completed_task.is_completed == False:
-        raise ValueError(f"{latest_completed_task.name} has not been completed.")
-    # previous dot_task is the dotted task before the last completed task
-    for task in tasks[latest_completed_task.index -1::-1]:
-        if task.is_completed == True:
-            continue
-        elif task.is_completed == False and task.is_dotted == False:
-            continue
-        elif task.is_completed == False and task.is_dotted == True:
-            previous_dot_task = task
-            return(previous_dot_task)
-        else:
-            raise ValueError(f"Task {task.name} metadata corrupted")
-    
 
-    
-
-
-def find_resume_from_task(tasks, latest_completed_task = None):
-    """ Returns resume_from_task from the task list
-    """
-    if latest_completed_task == None:
-        # TODO: sort out edge case
-        raise ValueError("No latest_completed_task")
-    if latest_completed_task.is_completed == False:
-        raise ValueError(f"{latest_completed_task.name} has not been completed.")
-
-    for task in tasks[latest_completed_task.index:]:
-        if task.is_completed == False and task.is_dotted == False:
-            resume_from_task = task
-            return(resume_from_task)
-        elif task.is_completed == False and task.is_dotted == True:
-            raise ValueError(f"Task {task.name} is dotted but not completed, and a task was completed before it.")
-        elif task.is_completed == True:
-            continue
-        else:
-            raise ValueError(f"Task {task.name} metadata corrupted")
 
 
         
